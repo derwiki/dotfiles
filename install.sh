6bfabdeb0e904d60b3e8948e21fc543f318a6f56
Adam Derewecki <adam@causes.com>
2012-10-14 22:18:57 +0000

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for df in `git ls-files | grep '^\'`; do
+  if [[ -e ~/$df ]]; then
+    echo $df exists, deleting
+    (cd ~ && rm $df)
+  fi
+  (cd ~ && ln -s dotfiles/$df $df)
+done
